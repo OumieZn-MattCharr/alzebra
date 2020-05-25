b826116640e4a92a22617ca08b158cdcb1f3627f
OumieZn-MattCharr <oumiezn.mattcharr@gmail.com>
2020-05-25 23:40:32 +0200

    add account information in setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ import setuptools
 with open ('README.md', 'r') as fh:
     long_description = fh.read ()
 
+NAME = 'alzebra'
+USERNAME = 'oumiezn-mattcharr'
+
 setuptools.setup (
-    name = 'Alzebra',
+    name = NAME,
     version = '0.0.1',
-    author = 'OumieZn&MattCharr',
-    author_email = '_',
+    author = USERNAME,
+    author_email = 'oumiezn.mattcharr@gmail.com',
     description = '',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    url = 'https://github.com/' + 'username' + '/' + 'name_project' + '.git',
+    url = 'https://github.com/' + USERNAME + '/' + NAME + '/',
     packages = setuptools.find_packages (),
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Operating System :: OS Independent',
     ],
-    python_requires = '>=3.6'
+    python_requires = '>=3.7'
 )
